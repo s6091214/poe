8e00c4b899299e5f8faf0b6a503ee78a4b56bd3c
tim <s6091214@gmail.com>
2022-10-14 22:05:25 +0800

    update

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+      <Component {...pageProps} />
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,51 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+// import Head from 'next/head'
+// import Image from 'next/image'
+import data from "../public/task.json";
+import { useBoolean } from "@chakra-ui/react";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
+  const { task } = data;
+  const [active, setActive] = useBoolean();
+
   return (
-    <div className={styles.container}>
-      <h1 className='text-3xl text-center font-bold mt-5'>
-        Hello World!!
-      </h1>
+    <div className={"flex justify-center flex-col items-center"}>
+      <div className="py-4">
+        {task.length > 0 &&
+          task.map((item, index) => {
+            const { title, content } = item;
+            return (
+              <div key={index}>
+                <h1
+                  className="text-3xl text-center font-bold mt-5"
+                  style={{ color: "#af6025" }}
+                >
+                  {title}
+                </h1>
+
+                {content.map((item, idx) => {
+                  return (
+                    <div className="flex items-center" key={idx}>
+                      <span>{`${idx + 1}.`}</span>
+                      <input
+                        className="mx-2"
+                        type="checkbox"
+                        name={`${title}_${idx}`}
+                        id={`${title}_${idx}`}
+                      />
+                      <label
+                        className="text-left text-xl cursor-pointer"
+                        for={`${title}_${idx}`}
+                      >
+                        {item}
+                      </label>
+                    </div>
+                  );
+                })}
+              </div>
+            );
+          })}
+      </div>
     </div>
-  )
+  );
 }
