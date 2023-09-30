11389bc08c25854f9a48c6d571648d850ebd6717
Hank Yang <hank.yang@molicity.com>
2023-09-30 20:38:40 +0800

    昇華輪播

--- pages/Gem.js
+++ pages/Gem.js
@@ -1,23 +1,23 @@
-import React from "react";
-import Image from "next/image";
-import data from "../public/gem.json";
+import React from 'react';
+import Image from 'next/image';
+import data from '../public/gem.json';
 
 const Gem = () => {
   const { awakened } = data;
   const type = awakened.map((item) => item.type);
-  const result = [...new Set(type)].join(",").split(",");
+  const result = [...new Set(type)].join(',').split(',');
   const res = [...new Set(result)];
 
   const Items = (props) => {
     const { color, type } = props;
     return (
-      <div className="px-2 inline-flex flex-col">
+      <div className="inline-flex flex-col px-2">
         {awakened.map((item) => {
           const { label, attributes, img, level } = item;
           return (
             attributes === type && (
               <div
-                className="text-xl inline-flex items-center"
+                className="inline-flex items-center text-xl"
                 style={{ color: color }}
                 key={label}
               >
@@ -31,13 +31,13 @@ const Gem = () => {
                   />
                 )}
                 {label}
-                <span className="text-white">({"LV" + level})</span>
+                <span className="text-white">({'LV' + level})</span>
               </div>
             )
           );
         })}
         <div>
-          {"數量"}
+          {'數量'}
           {awakened.filter(({ attributes }) => attributes === type).length}
         </div>
       </div>
@@ -45,21 +45,17 @@ const Gem = () => {
   };
 
   return (
-    <div
-      className={
-        "flex justify-center flex-col items-center bg-black bg-fixed bg-cover bg-poe bg-no-repeat pt-16"
-      }
-    >
+    <div className={'flex flex-col items-center justify-center pt-16'}>
       <div className="w-10/12">
-        <h1 className="text-2xl font-bold my-2 text-center">
-          {"覺醒寶石  "}
+        <h1 className="my-2 text-center text-2xl font-bold">
+          {'覺醒寶石  '}
           {`數量${awakened.length}`}
         </h1>
 
-        <div className="flex m-2 justify-evenly flex-wrap">
+        <div className="m-2 flex flex-wrap justify-evenly">
           {res.map((item) => {
             return (
-              <div key={item} className="p-2 bg-blue-900">
+              <div key={item} className="bg-blue-900 p-2">
                 {item}
               </div>
             );
@@ -67,9 +63,9 @@ const Gem = () => {
         </div>
 
         <div className="flex bg-gray-900 p-4">
-          <Items color={"#d20000"} type={"str"} />
-          <Items color={"#46a239"} type={"dex"} />
-          <Items color={"#8888ff"} type={"int"} />
+          <Items color={'#d20000'} type={'str'} />
+          <Items color={'#46a239'} type={'dex'} />
+          <Items color={'#8888ff'} type={'int'} />
         </div>
       </div>
     </div>
--- pages/Task.js
+++ pages/Task.js
@@ -1,6 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef, useCallback } from 'react';
 import Image from 'next/image';
 import JOBS from '../public/jobs.json';
+import styles from '../styles/Task.module.css';
 
 const jobs = [...JOBS].map((job, index) => ({ ...job, id: index + 1 }));
 
@@ -9,7 +10,32 @@ const Task = () => {
 
   const [classes, setClasses] = useState([]);
 
-  const [selectedClass, setActiveClass] = useState(null);
+  const [selectedClass, setActiveClass] = useState(0);
+
+  const carouselRef = useRef(null);
+
+  const [styleList, setStyleList] = useState([]);
+
+  const setStyle = (styles) => {
+    if (styles?.length) {
+      const resultSort = styles.sort((a, b) => a.cateIndex - b.cateIndex);
+      const result = resultSort.map((style) => {
+        const { xtrans, scale, zIndex, opacity } = style;
+        const transform = `translate(${xtrans - 50}%, ${
+          0 - 50
+        }%) scale(${scale})`;
+        return {
+          zIndex,
+          transform,
+          display: opacity === 0 ? 'none' : 'inline-block',
+        };
+      });
+      setStyleList(result);
+    }
+    return [];
+  };
+
+  const mod = (n, m) => ((n % m) + m) % m;
 
   useEffect(() => {
     if (selected) {
@@ -19,12 +45,105 @@ const Task = () => {
     }
   }, [selected]);
 
+  const arrangeHandler = (active) => {
+    if (typeof active === 'number' && active <= styleList.length) {
+      setActiveClass(active);
+    }
+  };
+
+  const arrange = (centerIndex) => {
+    const catesReference = carouselRef.current;
+    if (catesReference.children && classes?.length) {
+      setStyleList([]);
+      const half = (classes.length - 1) / 2;
+
+      const before = [];
+      for (let i = centerIndex - 1; before.length < half; i--) {
+        const cateIndex = mod(i, classes.length);
+        before.push({
+          cateIndex,
+          zIndex: 0,
+          xtrans: 0,
+          scale: 1,
+          opacity: 1,
+        });
+      }
+
+      const after = [];
+      for (
+        let i = centerIndex + 1;
+        after.length < classes.length - before.length - 1;
+        i++
+      ) {
+        const cateIndex = mod(i, classes.length);
+        after.push({
+          cateIndex,
+          zIndex: 0,
+          xtrans: 0,
+          scale: 1,
+          opacity: 1,
+        });
+      }
+
+      // console.log(before, after);
+
+      const centerZIndex = Math.max(before.length, after.length) + 1;
+      const beforeItems = beforeSetStyle(before, 0);
+      const afterItems = beforeSetStyle(after, 1);
+      const sequenceList = [
+        ...beforeItems,
+        {
+          cateIndex: centerIndex,
+          xtrans: 0,
+          scale: 1,
+          opacity: 1,
+          zIndex: centerZIndex,
+        },
+        ...afterItems,
+      ];
+
+      setStyle(sequenceList);
+
+      function beforeSetStyle(list, listIndex) {
+        let parentTrans = 0;
+        if (list?.length) {
+          return list.map((item, i) => {
+            const scale = 0.8 ** (i + 1);
+            const absolute = 105 * scale * 1.125 + parentTrans;
+            parentTrans = absolute;
+            const xtrans = (listIndex === 0 ? -1 : 1) * absolute;
+            // eslint-disable-next-line no-mixed-operators
+            const opacity = Math.max(1 - 0.25 * (i / 2 + 1) ** 2, 0);
+            const zIndex = Math.max(before.length, after.length) - i;
+            return {
+              cateIndex: item.cateIndex,
+              xtrans,
+              scale,
+              opacity,
+              zIndex,
+            };
+          });
+        }
+        return [];
+      }
+    }
+  };
+
+  useEffect(() => {
+    if (
+      carouselRef.current.children.length &&
+      typeof selectedClass === 'number'
+    ) {
+      arrange(selectedClass);
+    }
+  }, [carouselRef.current?.children, selectedClass]);
+
   return (
     <div className="container mx-auto text-center text-white">
       <h1 className="h-auto w-full py-4 text-center text-2xl text-yellow-300">
         職業
       </h1>
-      <ul className="flex w-full justify-center">
+      <ul className="mb-16 flex w-full justify-center">
         {jobs.map((job) => {
           return (
             <li
@@ -52,22 +171,33 @@ const Task = () => {
           );
         })}
       </ul>
-      <div className="carousel flex">
+      <div
+        className={`mx-0 flex justify-between ${styles.carousel}`}
+        ref={carouselRef}
+        style={{
+          minWidth: '100%',
+          position: 'relative',
+          zIndex: 10,
+          overflowX: 'visible',
+          padding: '130px 0',
+        }}
+      >
         {classes?.length
           ? classes.map((item, index) => {
               return (
                 <button
                   key={`class${index}`}
-                  className={`gametype side reset ${
-                    selectedClass === item ? 'center' : ''
+                  className={`${styles.gametype} side reset ${
+                    selectedClass === index ? styles.center : ''
                   }`}
-                  // :style="styleArray[index]"
-                  // @click="$emit('set-cate', cate.type)"
+                  style={{
+                    ...styleList[index],
+                  }}
+                  onClick={() => arrangeHandler(index)}
                 >
                   <iframe
                     src={`/images/classes/${item}.svg`}
-                    width="500"
-                    height="500"
+                    className="d-block pointer-events-none h-full w-full"
                   ></iframe>
                 </button>
               );
